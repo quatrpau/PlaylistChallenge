102d6b8151202f7a653aab19d5a07d3209f38059
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-11-21 16:11:21 -0500

    Finished lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,6 @@
 package io.zipcoder;
 
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +10,27 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int forward = startIndex;
+        int backward = startIndex;
+        int forwards = 0;
+        int backwards = 0;
+
+        while(!playList[forward].equals(selection) && !playList[backward].equals(selection)){
+            if(backward == 0){
+                backward = playList.length - 1;
+            }
+            else{
+                backward--;
+            }
+            backwards++;
+            if(forward == playList.length - 1){
+                forward = 0;
+            }
+            else{
+                forward++;
+            }
+            forwards++;
+        }
+        return Math.min(forwards,backwards);
     }
 }
